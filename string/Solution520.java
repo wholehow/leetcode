5c2ea1a30874358e098f700e518f9158e79b3baa
wholehow <zuoquan1993cn@gmail.com>
2017-07-01 16:01:23 +0800

    Update Solution520.java

--- string/Solution520.java
+++ string/Solution520.java
@@ -1,16 +1,16 @@
 package string;
 
-/* ���⣺
- * �жϵ����Ƿ�Ϊ����ĸ��д��ȫ����д����ȫ��Сд��
+/* 大意：
+ * 判断单词是否为首字母大写、全部大写或者全部小写。
  * */
 
 public class Solution520 {
 	
-	/* ������������һ�����ʣ������Ǽ���д��ʽ�Ƿ���ȷ�������Լٶ�ȷʵֻ������ĸ��
-	 * ��ô����Ҫ���ľ���ͳ�Ƴ����������д�д��ĸ�ĸ���cnt�������ж��Ƿ����������������
-	 * ���cntΪ0��˵������Сд����ȷ��
-	 * ���cnt�͵��ʳ�����ȣ�˵�����Ǵ�д����ȷ��
-	 * ���cntΪ1��������ĸΪ��д����ȷ���������������false
+	/* 这道题给了我们一个单词，让我们检测大写格式是否正确。（可以假定确实只含有字母）
+	 * 那么我们要做的就是统计出单词中所有大写字母的个数cnt，再来判断是否属于这三种情况，
+	 * 如果cnt为0，说明都是小写，正确；
+	 * 如果cnt和单词长度相等，说明都是大写，正确；
+	 * 如果cnt为1，且首字母为大写，正确，其他情况均返回false
 	 * */
 	
     public boolean detectCapitalUse(String word) {
@@ -20,7 +20,7 @@ public class Solution520 {
         int cnt = 0;
         for (Character c : word.toCharArray()) {
         	if (c <= 'Z') {
-        		cnt++;
+        	    cnt++;
         	}
         }
         return cnt == 0 || cnt == word.length() || (cnt == 1 && word.charAt(0) <= 'Z');
