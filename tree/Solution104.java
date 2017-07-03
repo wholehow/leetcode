b3f11afd01331fffe1b1d95fe3c957463a0ac860
wholehow <zuoquan1993cn@gmail.com>
2017-07-03 22:31:00 +0800

    Add files via upload

---
+++ tree/Solution104.java
@@ -0,0 +1,46 @@
+package tree;
+
+/* 求二叉树的深度
+ * */
+
+public class Solution104 {
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+    		return 0;
+    	}
+    	int lDepth = maxDepth(root.left);
+    	int rDepth = maxDepth(root.right);
+    	return Math.max(lDepth, rDepth) + 1;
+    }
+    
+    /*更简单点*/
+    /*public int maxDepth(TreeNode root) {
+    	if (root == null) {
+    		return 0;
+    	}
+    	return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }*/
+    
+    /*private int depth;
+
+    public int maxDepth(TreeNode root) {
+        depth = 0;
+        helper(root, 1);
+        
+        return depth;
+    }
+    
+    private void helper(TreeNode node, int curtDepth) {
+        if (node == null) {
+            return;
+        }
+        
+        if (curtDepth > depth) {
+            depth = curtDepth;
+        }
+        
+        helper(node.left, curtDepth + 1);
+        helper(node.right, curtDepth + 1);
+    }*/
+}
