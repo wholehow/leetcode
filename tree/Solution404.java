e1bb722cd2452db64674060a99f753309ce09158
wholehow <zuoquan1993cn@gmail.com>
2017-07-05 22:30:51 +0800

    Update Solution404.java

--- tree/Solution404.java
+++ tree/Solution404.java
@@ -1,14 +1,14 @@
 package tree;
 
-/* ����һ�ö�������������Ҷ�ӽ��ĺ͡�
+/* 计算一棵二叉树中所有左叶子结点的和。
  * */
 
 public class Solution404 {
 	
-	/* ע���Ǽ������е���Ҷ�ӽ����������е�Ҷ�ӽ�㡣
-	 * ���Թؼ�����������жϵ�ǰ����Ƿ�����Ҷ�ӽ�㣨��ʹ�ý���Ѿ���Ҷ�ӽ�㲻����������Ҷ�ӣ���
-	 * �������ڵ㣬����ڵ���û���ӽڵ㣬û�У�����Ҷ�ӽڵ㣩��ֱ������ʼֵȥ�ӣ������������ڵ�ݹ顣
-	 * ������ҽڵ㣬���ҽڵ����ӽڵ㣬����ҽڵ�ݹ飬���򲻹���û���ҽڵ㻹���ҽڵ�û���ӽڵ㣨����Ҷ�ӽڵ㣩��ֱ�ӿ�����0��
+	/* 注意是计算所有的左叶子结点而不是所有的叶子结点。
+	 * 所以关键就在于如何判断当前结点是否是左叶子结点（即使该结点已经是叶子结点不代表它是左叶子）。
+	 * 如果有左节点，看左节点有没有子节点，没有（即左叶子节点）则直接用起始值去加，有则继续对左节点递归。
+	 * 如果有右节点，且右节点有子节点，则对右节点递归，否则不管是没有右节点还是右节点没有子节点（即右叶子节点）都直接看做加0。
 	 * */
 	
 	public int sumOfLeftLeaves(TreeNode root) {
@@ -18,9 +18,8 @@ public class Solution404 {
 		if (root.left == null && root.right == null) {
 			return 0;
 		}
-		return ((root.left != null && root.left.left == null && root.left.right == null) ?
-				root.left.val : sumOfLeftLeaves(root.left)) + ((root.right != null &&
-						(root.right.left != null || root.right.right != null)) ?
-						sumOfLeftLeaves(root.right) : 0);
+		return ((root.left != null && root.left.left == null && root.left.right == null) ? root.left.val : sumOfLeftLeaves(root.left)) 
+			+ 
+			((root.right != null && (root.right.left != null || root.right.right != null)) ? sumOfLeftLeaves(root.right) : 0);
 	}
 }
