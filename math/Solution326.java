f760a9a22ac895c2c55d497bac964f7c0c5a6eb6
wholehow <zuoquan1993cn@gmail.com>
2017-07-08 22:07:14 +0800

    Update Solution326.java

--- math/Solution326.java
+++ math/Solution326.java
@@ -6,6 +6,22 @@ package math;
  * */
 
 public class Solution326 {
+	
+	/* 每次除3，判断最后是否为1。
+	 * */
+	
+    /*public boolean isPowerOfThree(int n) {
+        while (n > 0 && (n % 3 == 0)) {
+        	n /= 3;
+        }
+        return n == 1;
+    }*/
+	
+	/* n = 3^x ⇒ log3(n) = x
+	 * 要判断给出的整数是不是3的立方数，只用看x是不是整数就好了。而：
+	 * og3(n) = log10(n) / log10(3)
+	 * */
+	
     public boolean isPowerOfThree(int n) {
         return (n > 0 && (Math.log10(n) / Math.log10(3)) % 1 == 0);
     }
