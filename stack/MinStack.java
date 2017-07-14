6ab936f433e0bec5c0e39240c812636f08ee36f2
wholehow <zuoquan1993cn@gmail.com>
2017-07-14 23:48:17 +0800

    Update MinStack.java

--- stack/MinStack.java
+++ stack/MinStack.java
@@ -60,3 +60,12 @@ public class MinStack {
         return (int)min;
     }
 }
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
