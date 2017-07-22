c186f8f944a52b53df75a5f7c14b15d1fd2200ee
wholehow <zuoquan1993cn@gmail.com>
2017-07-22 23:12:43 +0800

    Update Solution401.java

--- bit_manipulation/Solution401.java
+++ bit_manipulation/Solution401.java
@@ -68,4 +68,20 @@ public class Solution401 {
         }
         return ans;
     }
+	
+	/*public List<String> readBinaryWatch(int num) {
+        List<String> times = new ArrayList<>();
+        for(int h = 0; h < 12; h++) {
+            int b1 = Integer.bitCount(h);
+            if (b1 > num) {
+            	continue;
+            }
+            for (int m = 0; m < 60; m++) {
+                if (Integer.bitCount(m) == num - b1) {
+                    times.add(h + ":" + (m < 10 ? "0" : "") + m);
+                }
+            }
+        }
+        return times;
+    }*/
 }
