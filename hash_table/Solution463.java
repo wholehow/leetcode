e2a220cb552e7f6f4ed490c7a748b327fcf222ab
wholehow <zuoquan1993cn@gmail.com>
2017-07-24 14:51:27 +0800

    Update Solution463.java

--- hash_table/Solution463.java
+++ hash_table/Solution463.java
@@ -1,23 +1,23 @@
 package hash_table;
 
-/* ���⣺
- * ����һ����ά����������ʽ�ĵ�ͼ������1��ʾ���أ�0��ʾˮ������Ԫˮƽ/��ֱ�����Խ��ߣ����ӡ�
- * ������ȫ��ˮ��Χ������������һ����������һ���������ӵĵ��浥Ԫ��������û�С���������ˮ��û�����ӵ����ϵ�ˮ����
- * ÿ����Ԫ���Ǿ��б߳�Ϊ1�������Ρ�����Ϊ���Σ����Ⱥ͸߶Ȳ�����100��ȷ�������ܳ���
- * ���磺[[0,1,0,0],
+/* 大意：
+ * 给出一个二维整数网格形式的地图，其中1表示土地，0表示水。网格单元水平/垂直（不对角线）连接。
+ * 网格完全被水包围，并且正好有一个岛（即，一个或多个连接的地面单元）。岛上没有“湖泊”（水内没有连接到岛上的水）。
+ * 每个单元格是具有边长为1的正方形。网格为矩形，宽度和高度不超过100。确定岛的周长。
+ * 例如：[[0,1,0,0],
  		[1,1,1,0],
  		[0,1,0,0],
  		[1,1,0,0]]
- 		���ӦΪ16��
+ 		结果应为16。
  * */
 
 public class Solution463 {
 	
-	/* Hash��������д���Ժ���
-	 * ��������ܳ����������
-	 * ��ͳɨ�跨����islands��¼��������neighbors��¼���е���������
-	 * �����ҵ����еĵ��죬islands++��Ȼ���жϵ�ǰ������·����ҷ��Ƿ�����ھ�neighbors++��
-	 * Ȼ��islands*4 - neighbors*2�������Ľ����ÿ��island����4���ߣ�ÿһ�����ڶ���ȥ���������ڵıߡ�
+	/* Hash方法不会写，以后补上
+	 * 计算的是周长不是面积！
+	 * 传统扫描法。用islands记录岛屿数，neighbors记录所有的相邻数。
+	 * 首先找到所有的岛屿，islands++；然后判断当前岛屿的下方和右方是否存在邻居neighbors++。
+	 * 然后islands*4 - neighbors*2就是最后的结果（每个island都有4条边，每一次相邻都会去掉两条相邻的边）。
 	 * */
 	
     public int islandPerimeter(int[][] grid) {
@@ -28,12 +28,12 @@ public class Solution463 {
         for (int i = 0; i < grid.length; i++) {
         	for (int j = 0; j < grid[i].length; j++) {
         		if (grid[i][j] == 1) {
-        			islands++;	//���㵺����
-        			// ����ֱ���²��ھ�
+        			islands++;	//计算岛屿数
+        			// 计算直接下侧邻居
             		if (i < grid.length - 1 && grid[i + 1][j] == 1) {
             			neighbors++;
             		}
-            		// ����ֱ���Ҳ��ھ�
+            		// 计算直接右侧邻居
             		if (j < grid[i].length - 1 && grid[i][j + 1] == 1) {
             			neighbors++;
             		}
