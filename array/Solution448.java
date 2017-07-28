eec7b0e7bc2588b8a6eedace9b00df18af7abfa1
wholehow <zuoquan1993cn@gmail.com>
2017-07-28 22:52:53 +0800

    Update Solution448.java

--- array/Solution448.java
+++ array/Solution448.java
@@ -2,24 +2,24 @@ package array;
 
 import java.util.*;
 
-/* ���⣺
-	����һ���������飬����1 �� a[i] �� n (n = ���鳤��)��һЩԪ�س������Σ������ĳ���һ�Ρ�
-	Ѱ������[1, n]��û�г����������е�Ԫ�ء�
-	���Բ�ʹ�ö���ռ䲢��O(n)����ʱ�����������Լ��践���б��������ռ�
+/* 大意：
+ * 给定一个整数数组，其中1 ≤ a[i] ≤ n (n = 数组长度)，一些元素出现两次，其他的出现一次。
+ * 寻找所有[1, n]中没有出现在数组中的元素。
+ * 可以不使用额外空间并在O(n)运行时间求解吗？你可以假设返回列表不算额外空间
  */
 
 public class Solution448 {	
 	
-	/* ���鳤��Ϊn��������Ԫ�ص�ȡֵ��ΧΪ[1, n]������index��ΧΪ[0, n - 1]��
-	 * Ҳ����˵�������Ԫ��û���ظ�����ô����ÿ��Ԫ��k������Ӧһ��index = k - 1.
-	 * ��˿�������������ʣ������ֵ�Ԫ������Ӧ��index�����Ԫ���ø���(�����ű�Ƿ�����ʵ��������������)
-	 * ������ʾ��index����Ӧ��Ԫ�ش��ڣ���nums[index] = - nums[index], ��ʾindex + 1 ������Ǵ��ڵģ���
-	 * �������������ÿ��Ԫ�ؽ����������֮���ٱ���һ�����飬���ĳ��index�ϵ�Ԫ��nums[index] �Ǹ���
-	 * ��˵����index����Ӧ��Ԫ��(index + 1)û�г��ֹ�����(index + 1)����Ҫ���ص�List��
+	/* 数组长度为n，数组中元素的取值范围为[1, n]，数组index范围为[0, n - 1]。
+	 * 也就是说如果数组元素没有重复，那么对于每个元素k，都对应一个index = k - 1.
+	 * 因此可以利用这个性质，将出现的元素所对应的index上面的元素置负，(正符号标记法，其实就是做个二分类)
+	 * 用来表示该index所对应的元素存在（将nums[index] = - nums[index], 表示index + 1 这个数是存在的）。
+	 * 遍历整个数组对每个元素进行这个操作之后，再遍历一遍数组，如果某个index上的元素nums[index] 非负，
+	 * 则说明该index所对应的元素(index + 1)没有出现过，则将(index + 1)加入要返回的List。
 	 * */
 	
 	 public List<Integer> findDisappearedNumbers(int[] nums) {
-		 //��Ŀ�Կӣ�ֻ����[]�������������NULL�����T-T
+		 //题目略坑，只接受[]的输出，不接受NULL的输出T-T
 		 List<Integer> resList = new ArrayList<Integer>();
 		 if (nums == null || nums.length == 0) {
 			 return resList;
