d6e43f875b0a311d4ffc8abf8470f30ab75e2cb7
wholehow <zuoquan1993cn@gmail.com>
2017-07-30 22:10:19 +0800

    Update Solution63.java

--- array/Solution63.java
+++ array/Solution63.java
@@ -1,49 +1,49 @@
 package array;
 
-/* ����62��
- * ���ڿ������������������һЩ�ϰ����ж��ٶ��ص�·���أ��ϰ���Ϳո�ֱ��������б��Ϊ1��0��
- * ���磺
+/* 延续62。
+ * 现在考虑如果网格中添加了一些障碍。有多少独特的路径呢？障碍物和空格分别在网格中标记为1和0。
+ * 例如：
  * [
   [0,0,0],
   [0,1,0],
   [0,0,0]
 ]
- * Ψһ·��������Ϊ2��
+ * 唯一路径的总数为2。
  * */
 
 public class Solution63 {
 	
-	/* ����62��Ľⷨ�������ڹ���dp�����ʱ����Ҫ���ݵ�ǰλ�õ�ֵ��0����1�ж���һ��������̡�
-	 * �������Ͻ�һ����1��������Ŀ����û�б�Ҫ����ȥ�ˡ�
-	 * Ȼ���ʼ����һ��/��һ�У����ݵ�ǰλ�õ�ֵ����Ϊ0����1��
-	 * ֮�����ÿһ������λ��0����1������¡����ս�����Ǳ��������½ǡ�
+	/* 延续62题的解法，就是在构建dp数组的时候需要根据当前位置的值是0还是1判断下一步构造过程。
+	 * 首先左上角一定是1，否则题目根本没有必要做下去了。
+	 * 然后初始化第一行/第一列，根据当前位置的值更新为0或者1。
+	 * 之后对于每一个其他位置0或者1有序更新。最终结果还是保存在右下角。
 	 * */
 	
-	/* ��ѹ��
+	/* 非压缩方法
 	 * */
 	
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
     	int m = obstacleGrid.length, n = obstacleGrid[0].length;
     	int[][] dp = new int[m][n];
     	dp[0][0] = obstacleGrid[0][0] ^ 1;
-    	// ��ʼ����һ��
+    	// 初始化第一行
     	for (int i = 1; i < m; i++) {
     		dp[i][0] = obstacleGrid[i][0] == 1 ? 0 : dp[i - 1][0];
     	}
-    	// ��ʼ����һ��
+    	// 初始化第一列
     	for (int j = 1; j < n; j++) {
     		dp[0][j] = obstacleGrid[0][j] == 1 ? 0 : dp[0][j - 1];
     	}
     	for (int i = 1; i < m; i++) {
     		for (int j = 1; j < n; j++) {
-    			// ���ݵ�ǰobstacleGridλ���Ƿ�Ϊ1�ж�dp[i][j]��θ���
+    			// 根据当前obstacleGrid位置是否为1判断dp[i][j]如何更新
     			dp[i][j] = obstacleGrid[i][j] == 1 ? 0 : dp[i -1][j] + dp[i][j - 1];
     		}
     	}
     	return dp[m - 1][n - 1];
     }
     
-    /* ѹ��
+    /* 压缩方法
      * */
     
     /*public int uniquePathsWithObstacles(int[][] obstacleGrid) {
