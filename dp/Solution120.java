66bd1399a1236502d6ecf6a5ff719e439ab97607
Zuo Quan <zuoquan1993cn@gmail.com>
2017-08-18 22:16:50 +0800

    Update Solution120.java

--- dp/Solution120.java
+++ dp/Solution120.java
@@ -1,39 +1,39 @@
 package dp;
 
-import java.awt.List;
+import java.util.List;
 
-/* ���⣺
- * ����һ�������Σ��Ӷ������ײ��ҵ���С·���͡�ÿ�����裬�����ƶ�����������ϵ��������֡�
- * ���磺
+/* 大意：
+ * 给定一个三角形，从顶部到底部找到最小路径和。每个步骤，可以移动到下面的行上的相邻数字。
+ * 例如：
  * [
      [2],
     [3,4],
    [6,5,7],
   [4,1,8,3]
 ]
-	��С·����Ϊ11��2 + 3 + 5 + 1 = 11
+	最小路径和为11，2 + 3 + 5 + 1 = 11
  * */
 
 public class Solution120 {
 	
-	/* ����ṹͦ������������Ԫ�ظ�����
-	 * �������ϼ��㣬����DP������⡣
+	/* 矩阵结构挺合理，行数即元素个数。
+	 * 从下向上计算，利用DP方法求解。
 	 * */
 	
     public int minimumTotal(List<List<Integer>> triangle) {
-    	// ��ʼpreRowΪ���һ��
+    	// 初始preRow为最后一行
         List<Integer> preRow = triangle.get(triangle.size() - 1); 
-        // ��-2��ʼ����ΪDPʱ��Ҫ�õ�size-1��
+        // 从-2开始是因为DP时需要用到size-1行
         for (int i = triangle.size() - 2; i >= 0; i--){
             List<Integer> curRow = triangle.get(i); 
             int preEle = preRow.get(0);
             for(int j = 0; j < curRow.size(); j++){
-            	// ÿ�ζ����ҵ���ǰ������С�����ֱ�����һ��
+            	// 每次都是找到当前行中最小数相加直至最后一行
                  curRow.set(j, curRow.get(j) + Math.min(preEle, preEle = preRow.get(j + 1)));
             }
             preRow = curRow;
         }
-        // ��һ�е�һ����Ҳ��Ψһһ�����������
+        // 第一行第一个，也是唯一一个就是最后结果
         return triangle.get(0).get(0);
     }
 }
